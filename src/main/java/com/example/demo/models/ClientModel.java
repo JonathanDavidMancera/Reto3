1ceec972c35ac043a67cf835936b43be4ac5e8cf
Jonathan Mancera <jonathan.mancera.mt@correo.usa.edu.co>
2021-11-10 14:16:32 -0500

    Tercer modificación

--- src/main/java/com/example/demo/models/ClientModel.java
+++ src/main/java/com/example/demo/models/ClientModel.java
@@ -1,16 +1,23 @@
 package com.example.demo.models;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "cliente")
-public class ClientModel {
-    
+public class ClientModel implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
@@ -20,36 +27,55 @@ public class ClientModel {
     private String email;
     private String password;
     private Long age;
-    
 
+    @OneToMany(cascade = { CascadeType.PERSIST }, mappedBy = "client")
+    @JsonIgnoreProperties("client")
+    private List<MessageModel> messages;
+
+    public List<MessageModel> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<MessageModel> messages) {
+        this.messages = messages;
+    }
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public Long getAge() {
         return age;
     }
+
     public void setAge(Long age) {
         this.age = age;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
--- src/main/java/com/example/demo/models/MessageModel.java
+++ src/main/java/com/example/demo/models/MessageModel.java
@@ -1,37 +1,56 @@
 package com.example.demo.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "mensaje")
-public class MessageModel {
-    
+public class MessageModel implements Serializable{
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
     private Long idMessage;
-    
+
     private String messageText;
 
+    @ManyToOne
+    @JoinColumn(name = "id")
+    @JsonIgnoreProperties("messages")
+    private ClientModel client;
+
+    public ClientModel getClient() {
+        return client;
+    }
+
+    public void setClient(ClientModel client) {
+        this.client = client;
+    }
 
     public Long getIdMessage() {
         return idMessage;
     }
+
     public void setIdMessage(Long idMessage) {
         this.idMessage = idMessage;
     }
+
     public String getMessageText() {
         return messageText;
     }
+
     public void setMessageText(String messageText) {
         this.messageText = messageText;
     }
-
-    
 
 }
